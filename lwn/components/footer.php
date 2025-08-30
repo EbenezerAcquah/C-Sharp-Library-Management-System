eba2aff524f5c630a0c3eab6e3c41ad8c0cbe71a
Ebenezer Acquah <ebenezeracquah000@gmail.com>
2025-08-29 17:07:42 -0700

    first commit

---
+++ lwn/components/footer.php
@@ -0,0 +1,42 @@
+<footer>
+        <div class="footer-container">
+            <div class="sec aboutus">
+                <h2>About Us</h2>
+                <p>The University of Mines and Technology (UMaT), Tarkwa, was established with the belief that 
+                    technology can change the world. The UMaT library is strategically positioned to help accomplish 
+                    this conviction by employing the library's considerable holdings and virtual online resources. 
+                    Helping users in this task are the skilled library staff.</p>
+                
+            </div>
+            <div class="sec quicklinks">
+                <h2>Quick Links</h2>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+            <div class="sec contactBx">
+                <h2>Contact Info</h2>
+                <ul class="info">
+                    <li>
+                        <span><i class='bx bxs-map'></i></span>
+                        <span>Ghana<br> Takoradi <br> UMaT-SRID</span>
+                    </li>
+                    <li>
+                        <span><i class='bx bx-envelope' ></i></span>
+                        <p><a href="#">ebenezeracquah000@gmail.com</a></p>
+                    </li>
+                    <li>
+                        <span style="opacity: 0.2;"><i class='bx bx-bug-alt'></i></span>
+                        <p style="opacity: 0.2;"> Ebenezer Acquah</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="./css-js/main.js"></script>
+</body>
+</html>
